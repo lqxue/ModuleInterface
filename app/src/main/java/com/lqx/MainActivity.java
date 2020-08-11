d910d846a2a0be683786d9dfa2d262d72adb804a
lqx <herolqx@126.com>
2020-08-11 17:07:43 +0800

    feat:创建APP壳工程

---
+++ app/src/main/java/com/lqx/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lqx;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * app 壳
+ *
+ * @author lqx Email:herolqx@126.com
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
